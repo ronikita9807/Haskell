1f41eeeb888e3016342942fd738bc16e395772d2
Catherine Galkina <anais94@yandex.ru>
2019-02-18 13:31:51 +0300

    Created template project

---
+++ app/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import MyProj
+
+main :: IO ()
+main = do
+  putStrLn "Hello World!"
+  runMyProj
---
+++ src/MyProj.hs
@@ -0,0 +1,6 @@
+module MyProj
+    ( runMyProj
+    ) where
+
+runMyProj :: IO ()
+runMyProj = putStrLn "This is a demo project."
