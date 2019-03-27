0c75955bbede764de6a5588c99a4d2e9cdb018c6
rooleg <rooleg@mail.ru>
2019-03-27 10:22:50 +0300

    speed

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,14 @@
 module Main where
 
 import MyProj
+import System.Random
 
 main :: IO ()
 main = do putStrLn "Hello, what's your name?"
           name <- getLine
-          putStrLn ("Hey " ++ name ++ "!")
+          putStrLn ("Welcome " ++ name ++ "!")
           putStrLn "Choose you`r profile (1,2,3,4)"
           prof <- getLine
           images <- loadImages
-          runMyProj name prof images
+          gen <- newStdGen
+          runMyProj gen name prof images
